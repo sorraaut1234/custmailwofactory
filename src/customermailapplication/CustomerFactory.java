0aee74ee6e818cf3e405180d76a1b1b19b47edc7
SORRAAUT RATTANACHAI <61050303@kmitl.ac.th>
2021-09-05 22:55:29 +0700

    CustomerFactory added

---
+++ src/customermailapplication/CustomerFactory.java
@@ -0,0 +1,16 @@
+package customermailapplication;
+
+public class CustomerFactory {
+        public static Customer createCustomer(String customerType) {
+            switch(customerType) {
+                case "Regular":
+                    return new RegularCustomer();
+                case "Mountain":
+                    return new MountainCustomer();
+                case "Delinquent":
+                    return new DelinquentCustomer();
+                default:
+                    return null;
+            }
+        }
+    }
--- src/customermailapplication/CustomerMailApplication.java
+++ src/customermailapplication/CustomerMailApplication.java
@@ -17,20 +17,20 @@ public class CustomerMailApplication {
     /**
      * @param args the command line arguments
      */
-    private Customer customer;
-    public void getCustomerTypeFromUser(String customerType) {
-        switch(customerType) {
-            case "Regular":
-                customer = new RegularCustomer();
-                break;
-            case "Mountain":
-                customer = new MountainCustomer();
-                break;
-            case "Delinquent":
-                customer = new DelinquentCustomer();
-                break;
-        }
-    }
+    private static Customer customer;
+//    public void getCustomerTypeFromUser(String customerType) {
+//        switch(customerType) {
+//            case "Regular":
+//                customer = new RegularCustomer();
+//                break;
+//            case "Mountain":
+//                customer = new MountainCustomer();
+//                break;
+//            case "Delinquent":
+//                customer = new DelinquentCustomer();
+//                break;
+//        }
+//    }
     public String generateMail() {
         return customer.createMail();
     }
@@ -40,18 +40,20 @@ public class CustomerMailApplication {
         Scanner inp = new Scanner(System.in);
         System.out.print("Please choose customer type 1. Regular, 2. Mountain, 3. Delinquent ");
         int type = inp.nextInt();
+        String customerType = "";
         switch(type) {
             case 1:
-                app.getCustomerTypeFromUser("Regular");
+                customerType = "Regular";
                 break;
             case 2:
-                app.getCustomerTypeFromUser("Mountain");
+                customerType = "Mountain";
                 break;
             case 3:
-                app.getCustomerTypeFromUser("Delinquent");
+                customerType = "Delinquent";
                 break;
             
         }
+        customer = CustomerFactory.createCustomer(customerType);
         System.out.println(app.generateMail());        
     }
 }
